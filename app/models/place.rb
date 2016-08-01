500f8ed4c66c585fdf12f685607197cbfe93b603
creeland <cree@provinsal.com>
2016-08-01 20:45:54 +0000

    Fixed Alert messages and added more validations

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ActiveRecord::Base
   belongs_to :user 
-  validates :name, presence: true
+  # Checks if a data is present in each form field before the data is submitted
+  # Also checks if the length meets the requirments 
+  validates :description, :address, :name, presence: true, length: {minimum: 3}
 end
