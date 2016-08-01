d1526ca119dc9409f40015a206c64c3b59842936
creeland <cree@provinsal.com>
2016-08-01 21:54:41 +0000

    Added geolocation support

--- Gemfile
+++ Gemfile
@@ -28,6 +28,10 @@ gem 'bootstrap-sass', '~> 3.3.5.1'
 gem 'sprockets', '2.11.0'
 # Use SCSS for stylesheets
 gem 'sass-rails'
+# Gem for maps integration
+gem 'geocoder', "1.3.4"
+# Sensitive info security
+gem 'figaro', ">= 1.0.0"
 
 # Use for pagination
 gem 'will_paginate', '~> 3.0.3'
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,10 @@
 class Place < ActiveRecord::Base
   belongs_to :user 
+
+  # Geocode
+  geocoded_by :address
+  after_validation :geocode # auto fetch cooridinates
+
   # Checks if a data is present in each form field before the data is submitted
   # Also checks if the length meets the requirments 
   validates :description, :address, :name, presence: true, length: {minimum: 3}
