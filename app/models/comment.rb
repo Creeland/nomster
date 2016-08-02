2e2ddc2376a5cf7761d185e6c6f037f511a55f95
creeland <cree@provinsal.com>
2016-08-02 00:17:38 +0000

    Added a comment rating dropdown

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,17 @@ class Comment < ActiveRecord::Base
   # designates that Comment is tied to user and place. 
   belongs_to :user
   belongs_to :place
+
+  RATINGS = {
+    'one star' => '1_star', 
+    'two stars' => '2_stars',
+    'three stars' => '3_stars',
+    'four stars' => '4_stars',
+    'five stars' => '5_stars'
+  }
+
+  def humanized_rating
+    RATINGS.invert[self.rating]
+  end
+
 end
