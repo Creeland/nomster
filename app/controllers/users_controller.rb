5b3bfcf71988ad8644a0f0abcca499d86287d065
creeland <cree@provinsal.com>
2016-08-05 21:14:10 +0000

    fleshed out user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
 
   def show 
+    # pulls the id from the url then queries the User table for the current user
+    @user = User.find(params[:id])
   end
 
 end
